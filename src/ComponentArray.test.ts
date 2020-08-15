b57a079ce071ec2a7f65b4be7f31a126b4574b60
Duknam Yoo <yoo2001818@gmail.com>
2020-08-15 10:38:31 +0900

    Implement ComponentArray

---
+++ src/ComponentArray.test.ts
@@ -0,0 +1,31 @@
+import { BaseComponentArray } from './ComponentArray';
+
+describe.each([
+  [
+    'BaseComponentArray',
+    () => new BaseComponentArray(
+      () => ({ value: 'a' }),
+      (from, to) => { to.value = from.value; },
+    ),
+  ],
+])('%s', (_, create) => {
+  it('should be able to maintain values', () => {
+    const array = create();
+    expect(() => array.get(0)).toThrow();
+    expect(array.size).toBe(0);
+    array.allocate(10);
+    expect(array.size).toBe(10);
+
+    array.get(3).value = 'abcd';
+    expect(array.get(3).value).toBe('abcd');
+  });
+  it('should be able to copy from/to values', () => {
+    const array = create();
+    array.allocate(5);
+
+    array.copyFrom(0, { value: 'a' });
+    const result = { value: '' };
+    array.copyTo(0, result);
+    expect(result.value).toBe('a');
+  });
+});
---
+++ src/ComponentArray.ts
@@ -0,0 +1,61 @@
+/**
+ * ComponentArray maintains an array of each component. It stores actual data
+ * of each entities.
+ *
+ * However, the entity itself is represented using EntityGroup. ComponentArray
+ * only stores component data. ComponentArray is nothing more than an expandable
+ * array, while EntityGroup specifies which offset to read/write. It can be
+ * understood as a block device; EntityGroup acts as a 'file allocation table'.
+ */
+export interface ComponentArray<T> {
+  size: number,
+  allocate(size: number): void,
+  get(pos: number): T,
+  copyFrom(pos: number, source: T): void,
+  copyTo(pos: number, target: T): void,
+}
+
+export class BaseComponentArray<T> implements ComponentArray<T> {
+  onCreate: () => T;
+
+  onCopy: (from: T, to: T) => void;
+
+  items: T[];
+
+  constructor(
+    onCreate: () => T,
+    onCopy: (from: T, to: T) => void,
+  ) {
+    this.onCreate = onCreate;
+    this.onCopy = onCopy;
+    this.items = [];
+  }
+
+  get size(): number {
+    return this.items.length;
+  }
+
+  allocate(size: number): void {
+    if (this.items.length >= size) return;
+    const prevPos = this.items.length;
+    this.items.length = size;
+    for (let i = prevPos; i < size; i += 1) {
+      this.items[i] = this.onCreate();
+    }
+  }
+
+  get(pos: number): T {
+    if (pos >= this.items.length) {
+      throw new Error('ComponentArray overflown');
+    }
+    return this.items[pos];
+  }
+
+  copyFrom(pos: number, source: T): void {
+    this.onCopy(source, this.get(pos));
+  }
+
+  copyTo(pos: number, target: T): void {
+    this.onCopy(this.get(pos), target);
+  }
+}
