1bf3c52a0ea8396414f11b560a17772f9baf0873
Duknam Yoo <yoo2001818@gmail.com>
2020-08-27 22:38:20 +0900

    Add entity

---
+++ src/Entity.ts
@@ -0,0 +1,16 @@
+import { EntityGroup } from './EntityGroup';
+import { EntityStore } from './EntityStore';
+
+export class Entity {
+  store: EntityStore;
+
+  group: EntityGroup;
+
+  index: number;
+
+  constructor(store: EntityStore, group: EntityGroup, index: number) {
+    this.store = store;
+    this.group = group;
+    this.index = index;
+  }
+}
