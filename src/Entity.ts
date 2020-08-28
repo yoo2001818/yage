fb150310b3440e6b53b096f0fe0c43af97c80afc
Duknam Yoo <yoo2001818@gmail.com>
2020-08-28 23:50:32 +0900

    Add entity methods

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,5 +1,6 @@
 import { EntityGroup } from './EntityGroup';
 import { EntityStore } from './EntityStore';
+import { Component } from './Component';
 
 export class Entity {
   store: EntityStore;
@@ -12,5 +13,57 @@ export class Entity {
     this.store = store;
     this.group = group;
     this.index = index;
+  }
+
+  // We have to maintain underlying structure from here.
+  float(): void {
+
+  }
+
+  unfloat(): void {
+
+  }
+
+  add<T>(component: Component<T> | string): void {
+
+  }
+
+  remove<T>(component: Component<T> | string): void {
+
+  }
+
+  has<T>(component: Component<T> | string): boolean {
+
+  }
+
+  get<T>(component: Component<T> | string): T {
+
+  }
+
+  destroy(): void {
+
+  }
+
+  getComponents(): Component<unknown>[] {
+  }
+  
+  set<T>(
+    component: Component<T> | string,
+    source: T,
+  ): void {
+
+  }
+
+  copyTo<T>(
+    component: Component<T> | string,
+    target: T,
+  ): void {
+
+  }
+
+  serialize(): unknown {
+  }
+
+  deserialize(value: unknown): void {
   }
 }
