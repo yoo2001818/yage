4a4a81aebeb07c9336b452df0edd456d52cbbbfd
Duknam Yoo <yoo2001818@gmail.com>
2020-08-29 23:58:07 +0900

    Implement entity floating

--- src/Entity.ts
+++ src/Entity.ts
@@ -17,19 +17,32 @@ export class Entity {
 
   // We have to maintain underlying structure from here.
   float(): void {
-
+    if (this.group.maxSize === 1) return;
+    const oldGroup = this.group;
+    // Create a single-sized entity group
+    const newGroup = this.store._createEntityGroupFrom(oldGroup, 1);
+    // TODO Copy oldGroup's contents onto newGroup
+    this.group = newGroup;
+    this.index = 0;
   }
 
   unfloat(): void {
-
+    if (this.group.maxSize !== 1) return;
+    // Find a large entity group
+    const oldGroup = this.group;
+    const newGroup = this.store._findEntityGroup(oldGroup.hashCode)
+      || this.store._createEntityGroupFrom(oldGroup, 32);
+    // TODO Add new entity onto the group
+    this.group = newGroup;
+    this.index = 0;
   }
 
   add<T>(component: Component<T> | string): void {
-
+    this.float();
   }
 
   remove<T>(component: Component<T> | string): void {
-
+    this.float();
   }
 
   has<T>(component: Component<T> | string): boolean {
