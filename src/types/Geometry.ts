ce63f16daa78cb3f69d80f5aed0a45ee3497363c
Duknam Yoo <yoo2001818@gmail.com>
2020-12-25 21:56:38 +0900

    Add channel geometry description

--- src/types/Geometry.ts
+++ src/types/Geometry.ts
@@ -12,6 +12,11 @@ export interface GeometryDescriptor {
   mode?: number,
 }
 
+// ChannelGeometry allows to specify separate indices for each attribute.
+// This therefore allows changing each triangle's property without changing
+// all the attributes, which is useful for calculating normals and tangents,
+// since position data is shared between all vertices, but hard normals are
+// not.
 export interface ChannelGeometryDescriptor {
   attributes: { [key: string]: GeometryAttribute | number[][] },
   indices: { [key: string]: number[] },
