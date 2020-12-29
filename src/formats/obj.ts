afb56fa8f4a705dfac64dd8c05cfebf3bea5e4bb
Duknam Yoo <yoo2001818@gmail.com>
2020-12-29 23:57:11 +0900

    Add obj entity

--- src/formats/obj.ts
+++ src/formats/obj.ts
@@ -1,4 +1,15 @@
-export function parseObj(input: string): unknown[] {
+import { Mesh } from '../render/Mesh';
+import { Material } from '../render/Material';
+import { GeometryDescriptor } from '../types/Geometry';
+
+export interface ObjEntity {
+  pos: number[],
+  geometry: GeometryDescriptor,
+  material: Material,
+  mesh: Mesh,
+}
+
+export function parseObj(input: string): ObjEntity[] {
   input.split('\n').forEach((line) => {
     if (line[0] === '#') return;
     const words = line.split(/\s+/g);
