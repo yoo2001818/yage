feeba10409cd7d9c8c16c3aec8e0287cbdf1ae56
Duknam Yoo <yoo2001818@gmail.com>
2021-01-28 23:59:05 +0900

    Implement BlenderControllerSystem update on init

--- src/input/BlenderControllerSystem.ts
+++ src/input/BlenderControllerSystem.ts
@@ -4,6 +4,8 @@ import { Component } from '../components/Component';
 import { EntityStore } from '../store/EntityStore';
 import { BlenderControllerTarget } from './BlenderControllerTarget';
 import { Transform } from '../render/Transform';
+import { EntityGroup } from '../store/EntityGroup';
+import { Entity } from '../store/Entity';
 
 export class BlenderControllerSystem {
   entityStore: EntityStore;
@@ -32,6 +34,8 @@ export class BlenderControllerSystem {
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
     this.handleMouseUp = this.handleMouseUp.bind(this);
+
+    this.targetComponent.subscribe(this.handleBlenderControllerChange);
   }
 
   enable(): void {
@@ -46,6 +50,17 @@ export class BlenderControllerSystem {
     this.enabled = false;
 
     this.canvas.removeEventListener('mousedown', this.handleMouseDown);
+  }
+
+  handleBlenderControllerChange(
+    group: EntityGroup,
+    start: number,
+    size: number,
+  ): void {
+    for (let i = 0; i < size; i += 1) {
+      const entity = this.entityStore.getEntityOfGroup(group, i + start);
+      this.updateEntity(entity);
+    }
   }
 
   handleMouseDown(e: MouseEvent): void {
@@ -71,27 +86,32 @@ export class BlenderControllerSystem {
       const entity = this.entityStore.getEntity(id);
       if (entity == null) return;
       const transform = entity.get<Transform>('transform');
-      const blenderController = entity.get<BlenderControllerTarget>(
-        'blenderController',
-      );
       const rot = quat.create();
       quat.rotateY(rot, rot, Math.PI / 180 * -deltaX / 4);
       quat.multiply(transform.rotation, rot, transform.rotation);
       quat.rotateX(transform.rotation, transform.rotation,
         Math.PI / 180 * -deltaY / 4);
-      // Move the object according to the rotation
-      vec3.transformQuat(
-        transform.position,
-        [0, 0, blenderController.distance],
-        transform.rotation,
-      );
-      vec3.add(
-        transform.position,
-        transform.position,
-        blenderController.center as [number, number, number],
-      );
-      entity.markChanged('transform');
+      this.updateEntity(entity);
     });
+  }
+
+  updateEntity(entity: Entity): void {
+    const transform = entity.get<Transform>('transform');
+    const blenderController = entity.get<BlenderControllerTarget>(
+      'blenderController',
+    );
+    // Move the object according to the rotation
+    vec3.transformQuat(
+      transform.position,
+      [0, 0, blenderController.distance],
+      transform.rotation,
+    );
+    vec3.add(
+      transform.position,
+      transform.position,
+      blenderController.center as [number, number, number],
+    );
+    entity.markChanged('transform');
   }
 
   handleMouseUp(): void {
