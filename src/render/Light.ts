603592bf4d6324027c8da7bcddeda28beef64326
Duknam Yoo <yoo2001818@gmail.com>
2021-02-11 23:58:21 +0900

    Add light type

--- src/render/Light.ts
+++ src/render/Light.ts
@@ -1,2 +1,9 @@
 export interface Light {
+  type: 'point' | 'directional' | 'spot',
+  color: number[],
+  ambient: number,
+  diffuse: number,
+  specular: number,
+  attenuation: number,
+  angle: number[],
 }
