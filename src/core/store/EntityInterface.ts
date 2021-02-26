fd6bed8c285a3e4280e36d0b1235bb48ae6b64b8
Duknam Yoo <yoo2001818@gmail.com>
2021-02-26 23:59:52 +0900

    Add entity interface

---
+++ src/core/store/EntityInterface.ts
@@ -0,0 +1,21 @@
+import { Component } from '../components/Component';
+import { ValueIsComponent, ValueOfComponent } from './types';
+
+export interface Entity<D extends ValueIsComponent<D> = any> {
+  id: number;
+
+  remove<K extends keyof D>(key: K): void;
+  remove<T>(component: Component<T> | string): void;
+  has<K extends keyof D>(key: K): boolean;
+  has<T>(component: Component<T> | string): boolean;
+  getPos<K extends keyof D>(key: K): number;
+  getPos<T>(component: Component<T> | string): number;
+  get<K extends keyof D>(key: K): ValueOfComponent<D[K]>;
+  get<T>(component: Component<T> | string): T;
+  set<K extends keyof D>(key: K, source: ValueOfComponent<D[K]>): void;
+  set<T>(component: Component<T> | string, source: T): void;
+  getComponents(): Component<unknown>[];
+  markChanged<T>(component: Component<T> | string): void;
+  toJSON(): unknown;
+  fromJSON(): unknown;
+}
